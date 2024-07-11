8d51e32bbcb2f0c64913f2af357d4cd77d848c3f
Pooja <poojadiliplad1999@gmail.com>
2024-07-11 12:23:54 +0530

    moviestoremvc

---
+++ MovieStoreMVC/Models/Domain/MovieGenre.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreMVC.Models.Domain
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; }
+        //[Required]
+        public int MovieId { get; set; }
+       /// <summary>
+       //[Required]
+     
+        public int GenreId { get; set; }
+       // [Required]
+
+    }
+}
